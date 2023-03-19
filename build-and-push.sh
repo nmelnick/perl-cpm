f0212920196061b617a73681a3914fc89f39e40a
Nicholas Melnick <nrmelnick@gmail.com>
2023-03-19 16:40:04 -0400

    Add a force pull to the builder

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,6 +7,6 @@ for VERSION in `cat versions.txt`; do
     echo "Building $VERSION$ITER"
     mkdir -p "$VERSION$ITER"
     cat Dockerfile.template | sed "s/%VERSION%/$VERSION$ITER/" > "$VERSION$ITER/Dockerfile"
-    cd "$VERSION$ITER" && docker buildx build --push --platform linux/amd64,linux/arm64 -t $DOCKER_REGISTRY/perl-cpm:$VERSION$ITER .; cd ..
+    cd "$VERSION$ITER" && docker buildx build --push --pull --platform linux/amd64,linux/arm64 -t $DOCKER_REGISTRY/perl-cpm:$VERSION$ITER .; cd ..
   done
 done
